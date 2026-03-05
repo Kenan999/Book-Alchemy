931f9b6c77075dedd7eeec4cc826abcb8e2732c2
Kenan Ali <55302817+Kenan999@users.noreply.github.com>
2026-03-05 08:15:24 +0100

    add database URI and data models structure

--- data_models.py
+++ data_models.py
@@ -3,18 +3,28 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 class Author(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(100), nullable=False)
+    birth_date = db.Column(db.String(50))
+    date_of_death = db.Column(db.String(50))
 
     def __repr__(self):
         return f'<Author {self.name}>'
 
+    def __str__(self):
+        return f"{self.name} (b. {self.birth_date})"
+
 class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    isbn = db.Column(db.String(50))
     title = db.Column(db.String(200), nullable=False)
+    publication_year = db.Column(db.Integer)
     author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
     
     author = db.relationship('Author', backref=db.backref('books', lazy=True))
 
     def __repr__(self):
         return f'<Book {self.title}>'
+
+    def __str__(self):
+        return f"'{self.title}' by {self.author.name if self.author else 'Unknown'}"
