83a574e21a85d781ae6672a6a15b87e43ed46836
Kenan Ali <55302817+Kenan999@users.noreply.github.com>
2026-03-05 08:14:08 +0100

    add database URI and data models structure

---
+++ data_models.py
@@ -0,0 +1,20 @@
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+class Author(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+
+    def __repr__(self):
+        return f'<Author {self.name}>'
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(200), nullable=False)
+    author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+    
+    author = db.relationship('Author', backref=db.backref('books', lazy=True))
+
+    def __repr__(self):
+        return f'<Book {self.title}>'
